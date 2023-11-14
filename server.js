3408b914daad97bed9cd5f08b70cfd9b625b184f
S-Hinkle <shcatchall@protonmail.com>
2023-11-14 15:46:06 -0500

    added routes for stock table. Setup HTML and CSS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import the necessary modules
 import express from 'express';
 import { promises as fs } from 'fs';
+import cors from 'cors';
 import pkg from 'pg';
 const { Pool } = pkg;
 
@@ -19,6 +20,9 @@ const app = express();
 // Use middleware to parse JSON bodies
 app.use(express.json());
 
+app.use(cors());
+
+app.use(express.static('public'));
 
 
 
@@ -85,6 +89,68 @@ app.delete('/users/:id', async (req, res) => {
 
 
 
+app.post('/stocks', async (req, res) => {
+    try {
+        const { name, ticker, shares, user_id } = req.body;
+        const newStock = await pool.query(
+            'INSERT INTO "stock" (name, ticker, shares, user_id) VALUES ($1, $2, $3, $4) RETURNING *',
+            [name, ticker, shares, user_id]
+        );
+        res.status(201).json(newStock.rows[0]);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/stocks', async (req, res) => {
+    try {
+        const allStocks = await pool.query('SELECT * FROM "stock"');
+        res.status(200).json(allStocks.rows);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/stocks/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const stock = await pool.query('SELECT * FROM "stock" WHERE id = $1', [id]);
+        if (stock.rows.length === 0) {
+            return res.status(404).send('Stock not found');
+        }
+        res.status(200).json(stock.rows[0]);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.put('/stocks/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, ticker, shares, user_id } = req.body;
+        const updatedStock = await pool.query(
+            'UPDATE "stock" SET name = $1, ticker = $2, shares = $3, user_id = $4 WHERE id = $5 RETURNING *',
+            [name, ticker, shares, user_id, id]
+        );
+        if (updatedStock.rows.length === 0) {
+            return res.status(404).send('Stock not found');
+        }
+        res.status(200).json(updatedStock.rows[0]);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// DELETE: Delete a user
+app.delete('/stocks/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query('DELETE FROM "stock" WHERE id = $1', [id]);
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 
 // Handle 404 for any other routes
